42a264d0b688101d4b123f6666471ae7d81e3052
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-04-13 18:02:02 +0200

    Initial commit of my 2D game for the portfolio

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC where I create my Machine Learning projects on Kaggle <a href="https://www.kaggle.com/oscaryezfeijo" target="_blank">here</a>. I work mostly in Python these days.
+    I also like Javascript. I've just installed Linux OS, so you can find me playing with Debian late at night. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/OYanez85" target="_blank">Github</a>!`,
+  "cs-degree": `This is my Master's Degree in Data Science and Artifical Intelligence. I hung it on the wall because I'm proud of it. It was a very practical degree and I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  I also make new chats on ChatGPT. You can check my ChatGPTs <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
+  You'll learn how I built this portfolio you're currently playing through!`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://www.linkedin.com/in/oscar-yanez-feijoo-28474688/" target="_blank">Check it out?</a>
+  Contact me at oscar.yanez.feijoo@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about my portfolio : On my portafolio you can find projects involving Machine Learning, PowerBI and Tableau. <a href="https://datavessel.wordpress.com/" target="_blank">here</a>.`,
+  library: `There are a lot of different books on my shelves. You can check them all on my Kaggle profile where I've created a SQLite database comprising all my library books <a href="https://www.kaggle.com/datasets/oscaryezfeijo/my-library" target="_blank">Check it out?</a>`,
+  exit: `If you want to exit Oscar's portfolio, just close the tab.`,
+};
